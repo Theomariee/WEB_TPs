3d443366706566c400cc7a5cf85afcfd250855ee
Theo MARIE <theo.marie5@gmail.com>
2019-03-13 19:40:46 +0100

    [TP1-ESIR-HELLO-WORLD-AVEC-COMPTAGE-REQUETE] Server counts requests since creation

--- TP1/index.js
+++ TP1/index.js
@@ -1,11 +1,16 @@
 const http = require('http')
+let nbRequests = 0
 
 const server = http.createServer((req,res) => {
     const jsonObject = {
-        'test': 'value'
+        'test': 'value',
+        'nbRequests': nbRequests
     }
+
+    nbRequests++
 
     res.setHeader('Content-Type', 'text/html')
     res.end(JSON.stringify(jsonObject))
 })
+
 server.listen(8000)

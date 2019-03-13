c0abeae213371451a19c9adebb31c7c0b30747ca
Theo MARIE <theo.marie5@gmail.com>
2019-03-13 19:19:25 +0100

    [TP1-ESIR-HELLO-WORLD] Wrote server that returns a JSON response

--- TP1/index.js
+++ TP1/index.js
@@ -1,1 +1,11 @@
-console.log("Hello ESIR")
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    const jsonObject = {
+        'test': 'value'
+    }
+
+    res.setHeader('Content-Type', 'text/html')
+    res.end(JSON.stringify(jsonObject))
+})
+server.listen(8000)

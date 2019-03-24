9d809b1ceada235145b9ce3964c6bc4024ea9a9e
Theo MARIE <theo.marie5@gmail.com>
2019-03-24 22:03:54 +0100

    [TP2-ESIR-STEP2] Implemented Step 2

--- TP2/index.js
+++ TP2/index.js
@@ -5,9 +5,19 @@ const fs = require('fs')
 const server = http.createServer((req,res) => {
     // Sync call to get files in current dir
     const filesInCurrentDir = fs.readdirSync('.')
+    const tFilesStats = []
+
+    filesInCurrentDir.forEach(f =>  {
+        const currentFile = {
+            name: f,
+            properties: fs.statSync('./' + f)
+        }
+
+        tFilesStats.push(currentFile)
+    })
 
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(filesInCurrentDir))
+    res.end(JSON.stringify(tFilesStats))
 })
 
 server.listen(8000)

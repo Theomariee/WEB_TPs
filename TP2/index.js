288eba4e5b22eafc42e9748d37426069e52c5a3b
Theo MARIE <theo.marie5@gmail.com>
2019-03-25 10:49:38 +0100

    [TP2-ESIR-STEP4] Async call to fs stats

--- TP2/index.js
+++ TP2/index.js
@@ -1,23 +1,14 @@
 const http = require('http')
-const fs = require('fs')
+const filestats = require('./lib/filestats')
 
 // Create server
 const server = http.createServer((req,res) => {
-    // Sync call to get files in current dir
-    const filesInCurrentDir = fs.readdirSync('.')
-    const tFilesStats = []
-
-    filesInCurrentDir.forEach(f =>  {
-        const currentFile = {
-            name: f,
-            properties: fs.statSync('./' + f)
-        }
-
-        tFilesStats.push(currentFile)
+    
+    filestats.getFileStats('.', (error, result) => {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(result))     
     })
 
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(tFilesStats))
 })
 
 server.listen(8000)

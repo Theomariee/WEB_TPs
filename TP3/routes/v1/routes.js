73cd1657cda47afd492b80cd39b0d84e5f3d3236
Theo MARIE <theo.marie5@gmail.com>
2019-03-25 17:50:09 +0100

    [TP3-ESIR-STEP3] Implemented module but cannot require succesfully

--- TP3/routes/v1/routes.js
+++ TP3/routes/v1/routes.js
@@ -1,24 +1,11 @@
 const express = require('express')
-const uuidv4 = require('uuid/v4')
 const bp = require('body-parser')
+const mod = require('../../modules/module.js')
 
-var router = express.Router()
 
-// Non-persistent list of users initialized with 2 members
-let userList = [
-    {
-        id: '5b449887-a5d5-49f3-b020-fd4afadfe024',
-        login: 'voodoo',
-        name: 'Marlon Kuqi',
-        age: 21
-    },
-    {
-        id: '95822075-33ad-4f1a-b16b-af4680725861',
-        login: 'chococo35',
-        name: 'Corentin Duchatelet',
-        age: 21
-    }
-]
+let router = express.Router()
+let module = new mod.Module()
+
 
 router.use(bp.json())
 router.use(bp.urlencoded({
@@ -27,60 +14,50 @@ router.use(bp.urlencoded({
 
 // Get list of all users 
 router.get('/users', function(req, res) {
-    res.send(userList)
+    res.send(module.getAll())
 })
 
 // Get user with specific id
 router.get('/users/:id', function(req, res) {
-    const userSearchedFor = userList.find(user => user.id == req.params.id)
-    if(!userSearchedFor) {
+    const user = module.get(req.params.id)
+    if(!user) {
         res.send('The id specified does not correspond to any user.')
     }
-    res.send(userSearchedFor)
+    res.send(user)
 })
 
 // Create a new user with the given params in the req.body
 router.post('/users', function(req, res){
     const newUser = req.body
-    if(!newUser) {
-        res.send('You must specify a login, a name and an age.')
+    const newUuid = module.add(newUser)
+    if(!newUuid) {
+        // TODO - Manage errors
+        res.send('Error.')
     }
-
-    const newUuid = uuidv4()
-    newUser.id = newUuid
-    userList.push(newUser)
-    
     res.send('Successfully created user with id ' + newUuid)
 })
 
 // Patch user with specific id with the given params in the req.body
 router.patch('/users/:id', function(req, res) {
-    const userSearchedFor = userList.find(user => user.id == req.params.id)
-    const tmpUserMemo = userSearchedFor
-
-    if(!userSearchedFor) {
-        res.send('The id specified does not correspond to any user.')
-    }
-
     const patchUser = req.body
-    Object.keys(patchUser).forEach(function(property) {
-        if(userSearchedFor[property]) {
-            userSearchedFor[property] = patchUser[property]
-        }
-    })
-    
-    // Make changes effective in userList
-    userList[tmpUserMemo] = userSearchedFor
+    const userIdToPatch = req.params.id
 
-    res.send('Successfully applied patch:' + JSON.stringify(userSearchedFor))
+    if(!module.update(userIdToPatch, patchUser)) {
+        // TODO - Manage errors
+        res.send('Error.')
+    }
+    
+    res.send('Successfully applied patch.')
 })
 
 router.delete('/users/:id', function(req, res) {
-    const indexUserSearchedFor = userList.findIndex(user => user.id == req.params.id)
-    if(indexUserSearchedFor == -1) {
-        res.send('The id specified does not correspond to any user.')
+    const userIdToRemove = req.params.id
+    if(!module.remove(userIdToRemove)) {
+        // TODO - Manage errors
+        res.send('Error.')
     }
-    userList.splice(indexUserSearchedFor, 1)
+
+    res.send('Successfully deleted user with id ' + userIdToRemove)
 })
 
 module.exports = router

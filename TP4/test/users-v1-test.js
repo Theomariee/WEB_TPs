9ba91f2a57bee969d753ae40740f8ca83658cebf
Theo MARIE <theo.marie5@gmail.com>
2019-04-01 01:01:42 +0200

    [TP4-ESIR-STEP1] Started writing GET and POST tests

--- TP4/test/users-v1-test.js
+++ TP4/test/users-v1-test.js
@@ -3,8 +3,37 @@ const chaiHttp = require('chai-http')
 const {app} = require('../app')
 chai.use(chaiHttp)
 
+const expect = chai.expect
+const assert = chai.assert
+const should = chai.should
+const requester = chai.request(app).keepOpen()
+
+const usersModel = require('../model/users')
+
 describe('Users tests', () => {
   it('should list ALL users on /v1/users GET', (done) => {
-    // TODO
+    requester
+      .get('/v1/users')
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res).to.have.status(200)
+      })
   })
+
+  it('should add a SINGLE user on /v1/users POST'), (done) => {
+    let user = {
+        name: 'Sanchez El Pueblo',
+        login: 'sanchezpueb',
+        age: 43
+    }
+
+    requester
+      .post('/v1/users')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        // TODO
+      })
+  }
 })

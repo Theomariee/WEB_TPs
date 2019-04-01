f261c49cbde10ce09f05a7a8d0f1febf5e4cd8b4
Theomariee <theo.marie5@gmail.com>
2019-04-01 15:44:56 +0200

    [TP4-ESIR-STEP3] Added helmet

--- TP4/app.js
+++ TP4/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 const usersRouter = require('./routes/users-v1')
 const usersModel = require('./model/users')
@@ -8,6 +9,7 @@ const app = express()
 const port = process.env.PORT || '3000'
 
 app.use(bodyParser.json())
+app.use(helmet())
 
 // On injecte le model dans le router. Ceci permet de supprimer la dépendance
 // directe entre le router et le modele
